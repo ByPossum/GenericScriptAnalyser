c40052deb8c9a51b53122637fe71d5941b124f82
Byron <bypossum@gmail.com>
2023-03-29 13:37:24 +0100

    Functionally decomposed window so init is nicer.

--- window.py
+++ window.py
@@ -6,13 +6,7 @@ from matplotlib import pyplot
 
 class window:
     def __init__(self, x, y, title):
-        self.window = tk.Tk()
-        self.window.title(title)
-        self.window.geometry(f'{x}x{y}+0+0')
-        self.create_inputs()
-        self.runButton = tk.Button(self.window, text="Analyse", command=self.analyse)
-        #self.runButton.pack()
-        self.runButton.grid(row=1, column=2)
+        self.createWindow(x, y, title)
         self.running = True
         self.window.protocol("WM_DELETE_WINDOW", self.close)
 
@@ -23,11 +17,16 @@ class window:
         self.window.destroy()
         self.running= False
 
+    def createWindow(self, x, y, title):
+        self.window = tk.Tk()
+        self.window.title(title)
+        self.window.geometry(f'{x}x{y}+0+0')
+        self.create_inputs()
+        self.createRunButton()
+
     def create_inputs(self):
         self.createEntriesandLabels()
         self.placeEntriesandLabels()
-        #self.packEntriesandLabels()
-
 
     def createEntriesandLabels(self):
         self.fpLabel = tk.Label(self.window, text="Folder Path")
@@ -41,11 +40,13 @@ class window:
         self.folderPath.grid(row=1, column=0)
         self.fileType.grid(row=1,column=1)
 
-    def packEntriesandLabels(self):
-        self.fpLabel.pack()
-        self.ftLabel.pack()
-        self.folderPath.pack()
-        self.fileType.pack()
+    def createRunButton(self):
+        self.runButton = tk.Button(self.window, text="Analyse", command=self.analyse)
+        self.runButton.grid(row=1, column=2)
+
+    def createAnalasysDropDown(self):
+        dropOptions = ['Length','Dependancy']
+        
 
     def analyse(self):
         scriptCollection = folderscraper(self.folderPath.get(), self.fileType.get())
